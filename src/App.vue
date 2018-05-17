7fc907109bb8c9665f740cb81ffbae0808a96125
linzefeng <57580053@qq.com>
2018-05-17 17:01:29 +0800

    circle list

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>hello vue.js</h1>
-    <h2>{{name}}</h2>
-    <h2>{{age}}</h2>
-    <h2>{{citise}}</h2>
-    <h2>{{password}}</h2>
-    <input @click="submit"> 点击获取1</input>
-    <input @click="submit2"> 点击获取2</input>
     <router-view></router-view>
     <!-- <router-view/>-->
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
-
   export default {
-    name: 'App',
-    data: function () {
-      return {
-        name: 'kaka',
-        age: '',
-        citise: [],
-        password: 'judian3321',
-      }
-    },
-    methods: {
-      submit: function () {
-        let result = null;
-        alert('hello haha');
-        axios.get('/static/mock/city.json')
-          .then(
-            res => {
-              let result = res.data.data.hotCities;
-              console.log(result[0]);
-              this.name = result[0].name;
-              this.age = result[0].spell;
-              this.citise = result;
-            }
-          )
-      },
-      clickHandler: function (firstName) {
-        this.age = firstName;
-      },
-      submit2: function () {
-        let result = null;
-        alert('hello kaka');
-        axios.get('/api/hello/kaka')
-          .then(
-            res => {
-              let pwd = res.data.data[0].password
-              console.log(pwd);
-              this.password = pwd;
-            }
-          )
-      },
-    }
+    name: 'App'
   };
 </script>
 <style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,64 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{password}}</h2>
+    <ol>
+      <li v-for="city in citise">
+        {{city.name}}
+      </li>
+    </ol>
+    <input @click="submit"> 点击获取1</input>
+    <input @click="submit2"> 点击获取2</input>
+    <Child></Child>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+
+  var Child = {
+    template: '<h1>自定义组件!</h1>'
+  }
   export default {
     name: 'home',
-    data () {
+    components: {Child},
+    data: function () {
       return {
+        citise: [],
+        password: 'judian3321',
         msg: 'hello.vue'
       }
+    },
+    methods: {
+      submit: function () {
+        let result = null;
+        alert('hello haha');
+        axios.get('/static/mock/city.json')
+          .then(
+            res => {
+              let result = res.data.data.hotCities;
+              console.log(result[0]);
+              this.name = result[0].name;
+              this.age = result[0].spell;
+              this.citise = result;
+            }
+          )
+      },
+      clickHandler: function (firstName) {
+        this.age = firstName;
+      },
+      submit2: function () {
+        let result = null;
+        alert('hello kaka');
+        axios.get('/api/hello/kaka')
+          .then(
+            res => {
+              let pwd = res.data.data[0].password
+              console.log(pwd);
+              this.password = pwd;
+            }
+          )
+      },
     }
   }
 </script>
