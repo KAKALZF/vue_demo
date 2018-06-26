73ed22f6fcdbdebadce3a983d214e7a05f491b6a
KAKALZF <575800503@qq.com>
2018-06-27 00:46:28 +0800

    page

--- src/components/page.vue
+++ src/components/page.vue
@@ -4,14 +4,14 @@
       <thead>
       <tr>
         <th>用户名</th>
-        <th>年龄</th>
+        <th>密码</th>
         <th>操作</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="user in userList">
-        <td> {{user.name}}</td>
-        <td>{{user.age}}</td>
+        <td> {{user.username}}</td>
+        <td>{{user.password}}</td>
         <td>
           <button type="button" class="btn btn-success" @click="add">增加</button>
           <button type="button" class="btn btn-warning">修改</button>
@@ -22,9 +22,9 @@
     </table>
     <ul class="pagination">
       <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item" v-for="n in totalPage">
+        <a class="page-link" @click="getUserList2($event)">{{n}}</a>
+      </li>
       <li class="page-item"><a class="page-link" href="#">Next</a></li>
     </ul>
   </div>
@@ -44,30 +44,54 @@
     name: 'page',
     data: function () {
       return {
-        userList: [{"name": "林", "age": 18}, {"name": "卡卡", "age": 17}],
+        userList: [],
+        page: 1,
+        size: 2,
+        totalPage: 0
       }
     },
     methods: {
-      submit: function () {
-        let result = null;
-        axios.get('/static/mock/city.json')
-          .then(
-            res => {
-              let result = res.data.data.hotCities;
-              console.log(result[0]);
-              this.username = result[0].name;
-              this.age = result[0].spell;
-              this.citise = result;
-            }
-          )
-      },
       add() {
         this.userList.push({"name": "kaka", "age": 22});
       },
       deleteUser() {
         this.userList.pop({"name": "kaka", "age": 22});
+      },
+      getUserList() {
+        axios({
+          method: 'get',
+          url: '/api/user/page',
+          params: {
+            page: this.page,
+            size: this.size
+
+          }
+        }).then(
+          res => {
+            console.log(res);
+            this.userList = res.data.data.content;
+            this.totalPage = res.data.data.totalPages;
+          })
+      },
+      getUserList2(event) {
+        console.log(event)
+        axios({
+          method: 'get',
+          url: '/api/user/page',
+          params: {
+            page: this.page,
+            size: this.size
+          }
+        }).then(
+          res => {
+//            console.log(res);
+            this.userList = res.data.data.content;
+            this.totalPage = res.data.data.totalPages;
+          })
       }
     },
-    mounted: {}
+    mounted() {
+      this.getUserList();
+    }
   }
 </script>
